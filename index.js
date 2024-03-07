4a441edb7bd3c5c5591f6d0d7caa9a13090652a1
spandankundu <99378538+spandankundu@users.noreply.github.com>
2024-03-07 10:57:09 +0530

    Update index.js

--- index.js
+++ index.js
@@ -18,9 +18,7 @@ db.once("open", () => {
 });
 
 const app = express();
-app.engine("ejs", ejsMate);
-app.set("view engine", "ejs");
-app.set("views", path.join(__dirname, "views"));
+
 app.use(express.urlencoded({ extended: true }));
 
 // Routes for MongoDB models
